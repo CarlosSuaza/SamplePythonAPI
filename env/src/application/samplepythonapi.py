f31f1101db7a2846df43688c6f58a5e7b9dab4f1
CarlosSuaza <Carlos_Suaza@hotmail.com>
2024-02-14 11:18:31 -0500

    USBCS202401- SamplePythonAPI

---
+++ env/src/application/samplepythonapi.py
@@ -0,0 +1,18 @@
+
+# samplepythonapi:
+
+from fastapi import FastAPI
+
+app: FastAPI = FastAPI(
+    title = "SamplePythonAPI",
+    description = "USBCS202401"
+)
+
+##############################################
+
+@app.get(
+    "/operacionGet",
+    summary = "Operacion Get",
+    tags=["Get"])
+async def operacion_get(dato_entrada: str):
+    return dato_entrada
